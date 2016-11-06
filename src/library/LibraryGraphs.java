f8b2ca106c1c15f2a12cfb7e05daeaf4f9b4ef77
FederFelipe <felipeoliveirafeder@hotmail.com>
2016-11-06 16:02:40 -0200

    BFS included in Library Graphics

--- src/library/LibraryGraphs.java
+++ src/library/LibraryGraphs.java
@@ -1,16 +1,22 @@
 package library;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * A class containing functions to manipulate graphs.
  */
 public class LibraryGraphs {
 
+	// Queue of BFS method
+	static Queue<Vertice> q = new LinkedList<Vertice>();
+	
 	/**
 	 * @brief Reads a file containing information about a graph as an adjacency
 	 *        list.
@@ -44,10 +50,10 @@ public class LibraryGraphs {
 					if (line.length == 3) {
 						e.add(new Edge(v.get(Integer.parseInt(line[0]) - 1), v.get(Integer.parseInt(line[1]) - 1),
 								Double.parseDouble(line[2])));
-					}
+					} 
 				}
 			} catch (NumberFormatException nfe) {
-				System.out.println("The file " + filePath + "doesn't match with the library standard");
+				System.out.println("The file " + filePath + " doesn't match with the library standard");
 				return null;
 			}
 			g.setE(e);
@@ -66,4 +72,37 @@ public class LibraryGraphs {
 		}
 		return g;
 	}
+	
+	/**
+	 * @brief BFS method for traversing and searching tree or graph data structures
+	 * @param Graph g, Vertice s
+	 *            The graph and a initial vertex.
+	 * @return void.
+	 */	
+	public void BFS(Graph g, Vertice s) {
+				
+		    List<Vertice> vet = g.getV();
+			Vertice ve = vet.get(s.getNumero());
+    
+			try{
+			q.offer(ve);
+			ve.setVisitado(true);
+			while(!(q.isEmpty())){
+				Vertice u = q.poll();
+				System.out.println("");
+				System.out.print( + u.getNumero() + " adjs : ");
+				for (Vertice v : u.adj) {
+					if(v.isVisitado() == false){
+						v.setPai(u);
+						v.setVisitado(true);
+						q.add(v);
+						System.out.print(v.getNumero() + " ");
+					}
+				}
+			}
+		 } catch(Exception e){
+			e.printStackTrace();
+		 }
+      }
+
 }
